bdaf433331730f4ee88fa90aa301a80c2ae1d0f3
Jakub Gilewicz <275409@student.pwr.edu.pl>
2023-06-12 10:14:31 +0200

    Poprawa ścieżek dostępu

--- Music.cpp
+++ Music.cpp
@@ -2,63 +2,63 @@
 #include "Music.h"
 
 Music::Music() {
-    if (!firePlace.openFromFile("/Shadow_Paths/WAVS/campfire.wav"))
+    if (!firePlace.openFromFile("../WAVS/campfire.wav"))
         std::cout << "ERROR";
     firePlace.setLoop(true);
     firePlace.setVolume(200);
 
-    if (!blacksmith.openFromFile("/Shadow_Paths/WAVS/blacksmith.wav"))
+    if (!blacksmith.openFromFile("../WAVS/blacksmith.wav"))
         std::cout << "ERROR";
     blacksmith.setLoop(true);
     blacksmith.setVolume(5);
 
-    if (!alchemist.openFromFile("/Shadow_Paths/WAVS/alchemist.wav"))
+    if (!alchemist.openFromFile("../WAVS/alchemist.wav"))
         std::cout << "ERROR";
     alchemist.setLoop(true);
     alchemist.setVolume(5);
 
-    if (!dungeon.openFromFile("/Shadow_Paths/WAVS/dungeon.wav"))
+    if (!dungeon.openFromFile("../WAVS/dungeon.wav"))
         std::cout << "ERROR";
     dungeon.setLoop(true);
     dungeon.setVolume(150);
 
-    if (!sigh.openFromFile("/Shadow_Paths/WAVS/sigh.wav"))
+    if (!sigh.openFromFile("../WAVS/sigh.wav"))
         std::cout << "ERROR";
 
-    if (!click.openFromFile("/Shadow_Paths/WAVS/click.wav"))
+    if (!click.openFromFile("../WAVS/click.wav"))
         std::cout << "ERROR";
 
-    if (!clock.openFromFile("/Shadow_Paths/WAVS/clock.wav"))
+    if (!clock.openFromFile("../WAVS/clock.wav"))
         std::cout << "ERROR";
 
     clock.setVolume(25);
 
-    if (!fight.openFromFile("/Shadow_Paths/WAVS/fight.wav"))
+    if (!fight.openFromFile("../WAVS/fight.wav"))
         std::cout << "ERROR";
     fight.setVolume(15);
 
-    if (!afterFightCom.openFromFile("/Shadow_Paths/WAVS/afterFightCom.wav"))
+    if (!afterFightCom.openFromFile("../WAVS/afterFightCom.wav"))
         std::cout << "ERROR";
     afterFightCom.setVolume(25);
 
-    if (!arrow.openFromFile("/Shadow_Paths/WAVS/Assets/arrow.wav"))
+    if (!arrow.openFromFile("../WAVS/Assets/arrow.wav"))
         std::cout << "ERROR";
     arrow.setVolume(50);
 
-    if (!blood.openFromFile("/Shadow_Paths/WAVS/Assets/blood.wav"))
+    if (!blood.openFromFile("../WAVS/Assets/blood.wav"))
         std::cout << "ERROR";
     blood.setVolume(15);
 
-    if (!monsterAttack.openFromFile("/Shadow_Paths/WAVS/Assets/monsterAttack.wav"))
+    if (!monsterAttack.openFromFile("../WAVS/Assets/monsterAttack.wav"))
         std::cout << "ERROR";
 
-    if (!banshee.openFromFile("/Shadow_Paths/WAVS/banshee.wav"))
+    if (!banshee.openFromFile("../WAVS/banshee.wav"))
         std::cout << "ERROR";
 
-    if (!goblinKing.openFromFile("/Shadow_Paths/WAVS/goblinKing.wav"))
+    if (!goblinKing.openFromFile("../WAVS/goblinKing.wav"))
         std::cout << "ERROR";
 
-    if (!abyssalWatchers.openFromFile("/Shadow_Paths/WAVS/abyssalWatchers.wav"))
+    if (!abyssalWatchers.openFromFile("../WAVS/abyssalWatchers.wav"))
         std::cout << "ERROR";
 
 
